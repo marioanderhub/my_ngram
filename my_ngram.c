343c306ae8d595c547d7a8a0160f312942075010
mario anderhub <anderhu_m@qwasar.io>
2023-11-06 23:27:22 +0000

    splitting up logic into seperate functions

--- my_ngram.c
+++ my_ngram.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 
-void count_chars(int *ngram, char *str) {
-    for (int i = 0; str[i]; i++) {
-        ngram[(unsigned int) str[i]] += 1;
+#define MAX_ARRAY_SIZE 128
+
+void fill_array(int *ngram, char *str) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        ngram[(int) str[i]] += 1;
     }
 }
 
-int main(int argc, char *argv[]) {
-    int ngram[128] = {0};
-    if (argc > 1) {
-        // iterate over args and count chars
-        for (int i = 1; i < argc; i++) {
-            count_chars(ngram, argv[i]);
-        }
-        // iterate ngram and print recorded values
-        for (int i = 0; i < 128; i++) {
+void print_array(int *ngram, int size) {
+    for (int i = 0; i < size; i++) {
             if (ngram[i] != 0)
                 printf("%c: %d\n", i, ngram[i]);
         }
+}
+
+int main(int argc, char *argv[]) {
+    int ngram[MAX_ARRAY_SIZE] = {0};
+    if (argc > 1) {
+        for (int i = 1; i < argc; i++) {
+            fill_array(ngram, argv[i]);
+        }
+        print_array(ngram, MAX_ARRAY_SIZE);
     }
     return 0;
 }
